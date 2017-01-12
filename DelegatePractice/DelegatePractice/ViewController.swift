6f38432937b92ed3d4442c3e03da79ca6b097df8
Jaemin <jaemin0216@gmail.com>
2017-01-12 15:17:55 +0900

    Make alertView

--- DelegatePractice/DelegatePractice/ViewController.swift
+++ DelegatePractice/DelegatePractice/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var selectedResult: UILabel!
+    
+    @IBAction func didAlert(_ sender: Any) {
+        // 알림창 인스턴스 생성
+        let alert = UIAlertView(title: "선택",
+                                message: "항목을 선택해주세요",
+                                delegate: nil,
+                                cancelButtonTitle: "취소",
+                                otherButtonTitles: "선택 A", "선택 B", "선택 C")
+        // 알림창 표시
+        alert.show()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
