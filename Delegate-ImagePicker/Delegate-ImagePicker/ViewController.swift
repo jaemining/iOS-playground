962859cebf8535b30fc4bd76108923eb15779c14
Jaemin <jaemin0216@gmail.com>
2017-01-13 15:20:52 +0900

    Wire up button and imageView

--- Delegate-ImagePicker/Delegate-ImagePicker/ViewController.swift
+++ Delegate-ImagePicker/Delegate-ImagePicker/ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet var imageView: UIImageView!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pick(_ sender: Any) {
+        
+        // 이미지 피커 컨트롤러 인스턴스 생성
+        let picker = UIImagePickerController()
+        
+        picker.sourceType = .photoLibrary // 이미지 소스로 사진 라이브러리 선택
+        picker.allowsEditing = true // 이미지 편집 기능 On
+        
+        self.present(picker, animated: false) // 이미지 피커 컨트롤러 실행     
     }
 
 
