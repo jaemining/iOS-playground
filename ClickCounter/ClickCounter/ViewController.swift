7f3498bb64c1161e78f46acf2664637b7d36d4c8
Jaemin <jaemin0216@gmail.com>
2017-01-17 13:15:36 +0900

    Transition to storyboard button

--- ClickCounter/ClickCounter/ViewController.swift
+++ ClickCounter/ClickCounter/ViewController.swift
@@ -16,24 +16,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Label
-        let label = UILabel()
-        label.frame = CGRect.init(x:150, y:150, width:60, height:60)
-        label.text = "0"
-        self.view.addSubview(label)
-        self.label = label
+//        // Label
+//        let label = UILabel()
+//        label.frame = CGRect.init(x:150, y:150, width:60, height:60)
+//        label.text = "0"
+//        self.view.addSubview(label)
+//        self.label = label
+//        
+//        // Button
+//        let button = UIButton()
+//        button.frame = CGRect.init(x:150, y:250, width:60, height:60)
+//        button.setTitle("Click", for: .normal)
+//        button.setTitleColor(UIColor.blue, for: .normal)
+//        self.view.addSubview(button)
         
-        // Button
-        let button = UIButton()
-        button.frame = CGRect.init(x:150, y:250, width:60, height:60)
-        button.setTitle("Click", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        self.view.addSubview(button)
-        
-        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
+//        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
     }
     
-    func incrementCount() {
+    @IBAction func incrementCount() {
         self.count = self.count + 1
         self.label.text = "\(self.count)"
     }
