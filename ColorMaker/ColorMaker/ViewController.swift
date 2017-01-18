e6186d86c287df5dcb678746ef8461376fca515d
Jaemin <jaemin0216@gmail.com>
2017-01-18 18:59:30 +0900

    Make ColorMaker

--- ColorMaker/ColorMaker/ViewController.swift
+++ ColorMaker/ColorMaker/ViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redControl: UISwitch!
+    @IBOutlet var greenControl: UISwitch!
+    @IBOutlet var blueControl: UISwitch!
+    @IBOutlet var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        changeColorComponent()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func changeColorComponent() {
+        
+        // Make sure the program doesn't crash if the controls aren't connected
+        if self.redControl == nil {
+            return
+        }
+        
+        let r: CGFloat = self.redControl.isOn ? 1 : 0
+        let g: CGFloat = self.greenControl.isOn ? 1 : 0
+        let b: CGFloat = self.blueControl.isOn ? 1 : 0
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
 
 
